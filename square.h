06fd31b83f5535724879fdca6050e646fd25aa1d
arstrue <90217981+arstrue@users.noreply.github.com>
2021-09-19 20:29:05 +0300

    Create square.h

---
+++ square.h
@@ -0,0 +1,14 @@
+#ifndef SQUARE_SQUARE_H
+#define SQUARE_SQUARE_H
+
+#endif //SQUARE_SQUARE_H
+
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+#include <stdlib.h>
+#define  _CRT_SECURE_NO_WARNINGS
+#define epsilon 1e-7///<const for comparing double
+#define a_ *(p + i*sizeof(double)*3 + 0*sizeof(double))
+#define b_ *(p + i*sizeof(double)*3 + 1*sizeof(double))
+#define c_ *(p + i*sizeof(double)*3 + 2*sizeof(double))
